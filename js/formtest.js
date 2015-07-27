5da0f7605f3f5de28e6acf11ffd5f98b545d8632
Zicheng Guo <guozicheng2013@gmail.com>
2015-07-27 14:31:14 +0800

    add new files

---
+++ js/formtest.js
@@ -0,0 +1,40 @@
+$(function() {
+    $(":input").focus(function() {
+        $(this).addClass("focus");
+    }).blur(function() {
+        $(this).removeClass("focus");
+    });
+    var $comment = $("#comment");
+    $("#bigger").click(function() {
+        if (!$comment.is(":animated")) {
+            if ($comment.height()<300)
+                {
+                    $comment.animate({height:"+=50"},600);
+                }else{
+                    alert("已经最大了！");
+                    return false;
+                }
+            }
+    });
+    $("#smaller").click(function() {
+        if (!$comment.is(":animated")) {
+            if ($comment.height()>50)
+                {
+                    $comment.animate({height:"-=50"},600);
+                }else{
+                    alert("不能再小了！");
+                    return false;
+                }
+            }
+    });
+    $("#up").click(function() {
+        if (!$comment.is(":animated")) {
+            $comment.animate({scrollTop:"-=50"},600);
+        };
+    });
+    $("#down").click(function() {
+        if(!$comment.is(":animated")) {
+            $comment.animate({scrollTop:"+=50"},600)
+        }
+    });
+});
